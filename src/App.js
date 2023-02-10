042655cff0158cdd53510636751deee48bfc3c5a
amanig3 <111730602+amanig3@users.noreply.github.com>
2023-02-10 09:24:05 +0100

    A

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Todos from './components/Todos';
+
+const todoStart = [
+  {
+    id:0,
+    title: "Hei",
+    content: "Hei Dette er en nettside"
+  }
+]
+
+function App() {
+  return (
+    <>
+    <Navbar username={"amanig"}/>
+    <Todos todoStart={todoStart}/>
+    </>
+  );
+}
+
+export default App;
